ce05a795a7fd1b1a2525c204741c24288b3d4365
mrgishi <34480146+mrgishi@users.noreply.github.com>
2020-03-23 12:16:22 +0900

    add feed_comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :micropost
   validates :content, presence: true, length: { maximum: 255 }
+  def feed_comments
+    Comment.where(user_id: self.following_ids + [self.id])
+  end
 end
